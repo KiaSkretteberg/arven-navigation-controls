4b506a812ee28327f27fe495f63cc0af92b1a91e
nmanhas2 <nubal.manhas1@gmail.com>
2023-03-26 15:12:53 -0600

    accounting for bumpsensor in pseudo navigation

--- arven.c
+++ arven.c
@@ -66,36 +66,99 @@ int main() {
         //dwm1001_request_position();
 
         //navigation testing stuff
-        
-        // while(obstacleCentre){
-        //     obstacleLeft = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_L_Duration, 10);
-        //     obstacleCentre = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_C_Duration, 10);
-        //     obstacleRight = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_R_Duration, 10);
-        //     //if an object is detected ONLY in front of arven, we would want it to reverse only
-        //     //if an object is detected in front and on the side, but not the back, we would want to reverse
-        //     if((!obstacleLeft && !obstacleRight && !bumpSensors) || (obstacleLeft && obstacleRight && !bumpSensors)){
-        //         //assuming Motor_FL = left, Motor_FR = right
-        //         //based on the xyz from the UWB module, we would set it to reverse left/right
-        //         //so we can go around the object, just setting it to reverse for now though
-        //         motor_reverse(Motor_FL, 30); //not sure what speed to set it to?
-        //         motor_reverse(Motor_FR, 30);
-        //     }
-        //     //if an object is detected in front of arven and to the left, we would want it to reverse right
-        //     if(obstacleLeft && !obstacleRight && !bumpSensors){
-        //         motor_reverse(Motor_FR, 30);
-        //         motor_reverse(Motor_FL, 10); //stopping probably isn't what we want, but not sure how we would reverse in a
-        //                               //left/right direction?
-        //     }
-        //     //if an object is detected in front of arven and to the right, we would want it to reverse left
-        //     if(!obstacleLeft && obstacleRight && !bumpSensors){
-        //         motor_reverse(Motor_FR, 10);
-        //         motor_reverse(Motor_FL, 30);
-        //     }
-        //     //if it's surrounded, just stop? 
-        //     if(obstacleLeft && obstacleRight && bumpSensors){
-        //         motor_stop(Motor_FL);
-        //         motor_stop(Motor_FR);
-        //     }
-        // }
+        /*
+        int turnSpeed = 30;
+        int normalSpeed = 10;
+        while(obstacleCentre){
+            obstacleLeft = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_L_Duration, normalSpeed);
+            obstacleCentre = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_C_Duration, normalSpeed);
+            obstacleRight = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_R_Duration, normalSpeed);
+            //if an object is detected ONLY in front of arven, we would want it to reverse only
+            //if an object is detected in front and on the side, but not the back, we would want to reverse
+            if((!obstacleLeft && !obstacleRight && !bumpSensors) || (obstacleLeft && obstacleRight && !bumpSensors)){
+                //assuming Motor_FL = left, Motor_FR = right
+                //based on the xyz from the UWB module, we would set it to reverse left/right
+                //so we can go around the object, just setting it to reverse for now though
+                motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+                //wait x amount of seconds to make sure it stopped
+                motor_reverse(Motor_FL, 30); //not sure what speed to set it to?
+                motor_reverse(Motor_FR, 30);
+            }
+            //if an object is detected in front of arven and to the left, we would want it to reverse right
+            if(obstacleLeft && !obstacleRight && !bumpSensors){
+                motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+                //wait x amount of seconds to make sure it stopped
+                motor_reverse(Motor_FR, turnSpeed);
+                motor_reverse(Motor_FL, normalSpeed); //stopping probably isn't what we want, but not sure how we would reverse in a
+                                      //left/right direction?
+            }
+            //if an object is detected in front of arven and to the right, we would want it to reverse left
+            if(!obstacleLeft && obstacleRight && !bumpSensors){
+                motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+                //wait x amount of seconds to make sure it stopped
+                motor_reverse(Motor_FR, normalSpeed);
+                motor_reverse(Motor_FL, turnSpeed);
+            }
+            //if it's surrounded, just stop? 
+            if(obstacleLeft && obstacleRight && bumpSensors){
+                motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+            }
+            //if an object is detected to in front and behind, not sure? 
+            //would probably depend on the UWB stuff for whether we turn right/left
+            //in terms of positioning
+            if(!obstacleLeft && !obstacleRight && bumpSensors){
+                //TODO
+            }
+            if(obstacleLeft && !obstacleRight && bumpSensors){
+                motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+                //wait x amount of seconds to make sure it stopped
+                motor_forward(Motor_FR, turnSpeed);
+                motor_forward(Motor_FL, normalSpeed);                
+            }
+            if(!obstacleLeft && obstacleRight && bumpSensors){
+                 motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+                //wait x amount of seconds to make sure it stopped
+                motor_forward(Motor_FR, normalSpeed);
+                motor_forward(Motor_FL, turnSpeed);                   
+            }
+        }
+        */
+       /*
+        while(obstacleRight){
+            obstacleLeft = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_L_Duration, normalSpeed);
+            obstacleCentre = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_C_Duration, normalSpeed);
+            obstacleRight = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_R_Duration, normalSpeed);
+            //if an object is detected ONLY in front of arven, we would want it to turn left and forward
+            if(!obstacleLeft && !obstacleCentre && !bumpSensors){
+                //assuming Motor_FL = left, Motor_FR = right
+                //based on the xyz from the UWB module, we would set it to reverse left/right
+                //so we can go around the object, just setting it to reverse for now though
+                motor_forward(Motor_FL, turnSpeed); //not sure what speed to set it to?
+                motor_forward(Motor_FR, normalSpeed);
+            }
+            //if an object is detected to the right, and the left, we would want it to just continue straight without turning
+            if(obstacleLeft && !obstacleCentre && !bumpSensors){
+                motor_forward(Motor_FR, normalSpeed);
+                motor_forward(Motor_FL, normalSpeed);
+            }
+            //if an object is detected to right, left and front, just reverse
+            if(obstacleLeft && obstacleCentre && !bumpSensors){
+                motor_reverse(Motor_FR, normalSpeed);
+                motor_reverse(Motor_FL, normalSpeed);
+            }
+            //if it's surrounded, just stop? 
+            if(obstacleLeft && obstacleCentre && bumpSensors){
+                motor_stop(Motor_FL);
+                motor_stop(Motor_FR);
+            }
+        */
+            
+        }       
     }
 }
