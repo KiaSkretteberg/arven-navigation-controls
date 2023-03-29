29918ea6e50163f8f0206eb16022a4a8f3752227
KiaSkretteberg <kia.skretteberg@canamtechnologies.com>
2023-03-29 14:44:38 -0600

    Copied code from pico-examples for wifi

--- web/web.h
+++ web/web.h
@@ -7,4 +7,14 @@
  * Author: Kia Skretteberg
  */
 
+#define WEB_CLIENT_SERVER        "worldtimeapi.org"
+#define WEB_CLIENT_HTTP_REQUEST  "GET /api HTTP/1.1\r\n" \
+                                 "Host: " WEB_CLIENT_SERVER "\r\n" \
+                                 "Connection: close\r\n" \
+                                 "\r\n"
+#define WEB_CLIENT_TIMEOUT_SECS  15
 
+typedef struct WEB_CLIENT_T_ {
+    struct altcp_pcb *pcb;
+    bool complete;
+} WEB_CLIENT_T;
