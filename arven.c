521a501dcd91f3b332918c9e6f099091d240ebc6
KiaSkretteberg <kia.skretteberg@canamtechnologies.com>
2023-04-19 20:15:55 -0600

    Restructured the code a little bit to be cleaner and easier to follow as well as reliant on a schedule in order to run

--- arven.c
+++ arven.c
@@ -12,9 +12,10 @@
 #include "ir.h"
 #include "web.h"
 
-// const uint BLUE_LED_PIN = 15;
-// const uint YELLOW_LED_PIN = 16;
-// const uint GREEN_LED_PIN = 17;
+/************************************************************************/
+/* Global Variables                                                     */
+/************************************************************************/
+
 const char WIFI_NETWORK_NAME[] = "PH1";
 const char WIFI_PASSWORD[] = "12345678";
 
@@ -23,7 +24,13 @@ volatile MotorDirection currentMotorDirection_L = Motor_Stopped;
 volatile MotorDirection currentMotorDirection_R = Motor_Stopped;
 
 const int SPEED = 40;
+const int TURN_SPEED = 50;
 
+/************************************************************************/
+/* Local Definitions (private functions)                                */
+/************************************************************************/
+
+void find_user(struct AtmegaSensorValues sensorValues);
 void turn_right();
 void turn_left();
 void go_forward();
@@ -40,32 +47,26 @@ int main() {
     int numDoses = 0;
     
     stdio_init_all();
-    
-    // gpio_init(BLUE_LED_PIN);
-    // gpio_set_dir(BLUE_LED_PIN, GPIO_OUT);
-    
-    // gpio_init(YELLOW_LED_PIN);
-    // gpio_set_dir(YELLOW_LED_PIN, GPIO_OUT);
-    
-    // gpio_init(GREEN_LED_PIN);
-    // gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
 
     dwm1001_init_communication();
     atmega_init_communication();
-
     motor_init_all();
 
+    // wait 2seconds to allow debugging connection
     sleep_ms(2000);
 
     web_init(WIFI_NETWORK_NAME, WIFI_PASSWORD, "Arven", NULL, NULL, NULL);
 
-    while (true) {
+    while (true) 
+    {
+        // if we don't have a schedule, check for a schedule
         if(scheduleId == -1)
         {
             web_request_check_schedule();
             // capture the schedule id once the web request has finished
             scheduleId = web_response_check_schedule();
         }
+        // we have a schedule, we need to find the user and check dosage stats
         else
         {
             if(numDoses <= 0) 
@@ -79,210 +80,138 @@ int main() {
                     Weight_DetermineDosage(startingWeight, numDoses);
                 }
             }
-        }
 
-        // TODO: all of the code below should only run if schedule_id != -1
-        // retrieve current frame 
-        sensorValues = atmega_retrieve_sensor_values();
-
-        if(!startingWeight && scheduleId != -1) 
-        {
-            startingWeight = Weight_CalculateMass(sensorValues.Weight);
-            web_request_retrieve_dose_stats(scheduleId);
-        }
-
-        // only check the values below and act on them if SOMETHING has changed 
-        // (also used as an indication of a proper frame received)
-        if(sensorValues.Changes) 
-        {        
-            Weight_LoadState newLoadState = Weight_CheckForLoad(sensorValues.Weight); 
-
-            if(newLoadState == Weight_LoadPresent && newLoadState != previousLoadState) 
+            // if we don't have a starting weight, log the current weight as the starting weight
+            // then determine how much a single dose should weight based on amount of doses remaining
+            if(!startingWeight) 
             {
-                Weight_Change change = Weight_CheckForChange(sensorValues.Weight);
-
-                switch(change)
-                {
-                    case Weight_RefillChange:
-                        
-                        // determine the new starting weight
-                        startingWeight = Weight_CalculateMass(sensorValues.Weight);
-                        numDoses = 0;
-                        web_request_retrieve_dose_stats(scheduleId);
-                        break;
-                    case Weight_LargeChange:
-                        // TODO: log an event because this change was a possible concern
-                    case Weight_SmallChange:
-                        web_request_log_delivery(scheduleId);
-                        break;
-                    default:
-                        break;
-                }
+                startingWeight = Weight_CalculateMass(sensorValues.Weight);
+                web_request_retrieve_dose_stats(scheduleId);
             }
-            // update the load state for later comparison (only if it's not an error)
-            if(newLoadState != Weight_LoadError) previousLoadState = newLoadState;
 
-            // check if there is an obstacle within 30cm
-            bool obstacleLeft = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_L_Duration, 20);
-            bool obstacleCentre = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_C_Duration, 20);
-            bool obstacleRight = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_R_Duration, 20);
+            // get the frame info from the atmega
+            // TODO: We should toggle control of the atmega code detecting the sensors based on if we want data
+            sensorValues = atmega_retrieve_sensor_values();
+            
+            // if there were changes in the sensor values, react to them
+            //TODO: This should detect specific changes but right now it's hardcoded and essentially ignored
+            if(sensorValues.Changes) 
+            {        
+                // Check if we currently have something on the weight sensor
+                Weight_LoadState newLoadState = Weight_CheckForLoad(sensorValues.Weight); 
 
-            // Check if the ground (50mm -- 5cm) is still there
-            bool dropImminentLeft = IR_CheckForDrop(sensorValues.IR_L_Distance, 60); 
-            bool dropImminentRight = IR_CheckForDrop(sensorValues.IR_R_Distance, 60);
+                // if the state changed from something removed, to something added, check to see 
+                // how much the weight changed from the last time there was weight
+                if(newLoadState == Weight_LoadPresent && newLoadState != previousLoadState) 
+                {
+                    // check how big the change was
+                    Weight_Change change = Weight_CheckForChange(sensorValues.Weight);
 
-            bool obstacleRear = sensorValues.Bump_L || sensorValues.Bump_R;
-        
-            //struct DWM1001_Position position = dwm1001_request_position();
-            // TODO: retrieve the user location into somewhere in order to run comparisons against our position
-            //web_request("/get_user_location");
-            //pseudo navigation/object detection stuff
-
-            //prioritize drop detection first
-            if(!dropImminentLeft && !dropImminentRight){
-                // printf("\nno drop");
-                if(!obstacleCentre && !obstacleLeft && !obstacleRight && !obstacleRear) {
-                    printf("\nno obstacle");
-                    go_forward(); 
-                } else {
-                    //printf("\nobstacle");
-                    //second priority is bump sensors, that way we know whether or not we can reverse
-                    //reverse = okay
-                    if(!obstacleRear){
-                        //printf("\nno obstacle behind");
-                        //third priority is whether or not something in front of arven is detected
-                        //something in front = reverse left/right/straight, nothing in front = keep going straight
-                        // and take turns depending on the UWB location
-                        //
-                        //no object behind, but there is an object in front
-                        if(obstacleCentre){                         //depends on space between object and arven
-                            if((!obstacleLeft && !obstacleRight) || (obstacleLeft && obstacleRight)){
-                                printf("\nobstacle front");
-                                //assuming Motor_FL = left, Motor_FR = right
-                                //based on the xyz from the UWB module, we would set it to reverse left/right
-                                //so we can go around the object, just setting it to reverse for now though
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-
-                                //wait x amount of seconds to make sure it stopped
-                                go_backward();
-                            }
-                            //if an object is detected in front of arven and to the left, we would want it to reverse right
-                            if(obstacleLeft && !obstacleRight){
-                                printf("\nobstacle front and left");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                turn_right();
-                            }
-                            //if an object is detected in front of arven and to the right, we would want it to reverse left
-                            if(!obstacleLeft && obstacleRight){
-                                printf("\nobstacle front and right");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                turn_left();
-                            }
-                        //no object behind or in front
-                        } else{
-                            //if an object is detected left, and right, continue straight or reverse?
-                            //probably depends on how much space is in between arven and the objects, etc
-                            if(obstacleLeft && obstacleRight){
-                                printf("\nobstacle left and right");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                go_forward();
-                            }
-                            //turn right if we see an object on the left
-                            if(obstacleLeft && !obstacleRight){
-                                printf("\nobstacle left");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                turn_right();                  
-                            }
-                            //turn left if there's only an object on the right
-                            if(!obstacleLeft && obstacleRight){
-                                printf("\nobstacle right");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                turn_left();   
-
-                            }
-                            //keep going forward if we detect no objects
-                            if(!obstacleLeft && !obstacleRight){
-                                printf("\nno obstacles");
-                                go_forward();                      
-                            }
-                        }
-                    }
-                    //object detected behind
-                    if(obstacleRear){
-                        // printf("\nobstacle behind");
-                        //not sure what to do if it's behind + in front? since you'd have to turn
-                        //in such a way that there's a enough space in front/behind, which would 
-                        //determine whether you reverse or go forward, etc
-                        if(obstacleCentre){
-                        // printf("\nobstacle centre");
-                            //if it's surrounded, just stop? 
-                            if(obstacleLeft && obstacleRight){
-                                printf("\nobstacles everywhere");
-                                stop();
-                                //wait x amount of seconds to make sure it stopped
-                            }
-                            if(obstacleLeft && !obstacleRight){
-                                printf("\nobstacle behind, front, and left");
-                            }
-                            if(!obstacleLeft && obstacleRight){
-                                printf("\nobstacle behind, front, and right");
-                            }                          
-                        //something behind, nothing in front
-                        } else{
-                            // printf("\nno obstacle behind");
-                            //nothing right/left
-                            if(!obstacleLeft && !obstacleRight){
-                                printf("\nobstacle behind");
-                                go_forward();                             
-                            }
-                            if(obstacleLeft && !obstacleRight){
-                                printf("\nobstacle left and behind");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                turn_right();                            
-                            }
-                            if(!obstacleLeft && obstacleRight){
-                                printf("\nobstacle right and behind");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                turn_left();                            
-                            }
-                            //something behind, left, and right, go straight? depends on space
-                            if(obstacleLeft && obstacleRight){
-                                printf("\nobstacle left, right, behind");
-                                // motor_stop(Motor_FL);
-                                // motor_stop(Motor_FR);
-                                //wait x amount of seconds to make sure it stopped
-                                go_forward();                          
-                            }
-                        }
+                    switch(change)
+                    {
+                        // the change increased the weight so the medication was refilled
+                        case Weight_RefillChange:
+                            // determine the new starting weight
+                            startingWeight = Weight_CalculateMass(sensorValues.Weight);
+                            numDoses = 0;
+                            //TODO: The api requests needs to log a refill somehow
+                            web_request_retrieve_dose_stats(scheduleId);
+                            break;
+                        // the change was too big and may indicate an overdose, but we'll still log a delivery
+                        case Weight_LargeChange:
+                            // TODO: log an event because this change was a possible concern
+                        // the change was normal, just log a delivery
+                        case Weight_SmallChange:
+                            web_request_log_delivery(scheduleId);
+                            break;
+                        // any other change doesn't matter and may have been a hiccup
+                        default:
+                            break;
                     }
                 }
-            } else{
-                printf("\ndrop");
-                //just stop if we're about to fall
-                stop();           
+                // update the load state for later comparison (only if it's not an error)
+                if(newLoadState != Weight_LoadError) previousLoadState = newLoadState;
+
+                //find_user(sensorValues);
             }
         }
     }
 }
 
+/************************************************************************/
+/* Local  Implementation                                                */
+/************************************************************************/
+
+void find_user(struct AtmegaSensorValues sensorValues)
+{
+    // check if there is an obstacle within 30cm
+    bool obstacleLeft = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_L_Duration, 30);
+    bool obstacleCentre = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_C_Duration, 30);
+    bool obstacleRight = Ultrasonic_CheckForObstacle(sensorValues.Ultrasonic_R_Duration, 30);
+
+    // Check if the ground (60mm -- 6cm) is still there
+    bool dropImminentLeft = IR_CheckForDrop(sensorValues.IR_L_Distance, 60); 
+    bool dropImminentRight = IR_CheckForDrop(sensorValues.IR_R_Distance, 60);
+
+    // Check if any of the backup sensors are being pressed
+    bool obstacleRear = sensorValues.Bump_L || sensorValues.Bump_R;
+
+    //struct DWM1001_Position position = dwm1001_request_position();
+    // TODO: retrieve the user location into somewhere in order to run comparisons against our position
+    //web_request("/get_user_location");
+    //TODO: We need a function to parse out the user's location into DWM1001_Position
+
+    // drop imminent
+    if(dropImminentLeft || dropImminentRight)
+    {
+        printf("\ndrop");
+        //just stop if we're about to fall
+        stop();
+        //TODO: We may want to backup/turn if only one has a drop pending  
+        //TODO: If we sit here too long, report a failed delivery         
+    }
+    // no drop, proceed to check other sensors
+    else
+    {
+        // no obstacles in front, so go forward
+        if(!obstacleCentre && !obstacleLeft && !obstacleRight) 
+        {
+            printf("\nno obstacle");
+            go_forward(); 
+        } 
+        // obstacle directly in front, and to the left, so turn right
+        else if(obstacleCentre && obstacleLeft)
+        {
+            turn_right();
+        }
+        // obstacle directly in front, and to the right, so turn left
+        else if (obstacleCentre && obstacleRight)
+        {
+            turn_left();
+        }
+        // just an obstacle in front, so turn towards the destination
+        else if (obstacleCentre && !obstacleRight && !obstacleLeft)
+        {
+            //TODO: pick a direction that takes us closer to the destination
+            turn_right();
+        }
+        // obstacles all in front, but no rear, so backup
+        else if (obstacleCentre && obstacleRight && obstacleLeft && !obstacleRear)
+        {
+            go_backward();
+        }
+        // obstacles everywhere, just stop
+        else
+        {
+            stop();
+            //TODO: If we sit here too long, report a failed delivery
+        }
+    } 
+}
+
+
 void turn_right()
 {
-
     if(currentMotorDirection_R != Motor_Reverse)
     {
         printf("\ngo backward right");
